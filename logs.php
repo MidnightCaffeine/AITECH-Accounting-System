516aac483a8dd9f938a07e8473f54b17948f2c8f
MidnightCaffeine <jobert.simbre14@gmail.com>
2023-11-17 09:09:21 +0800

    admin side done

--- logs.php
+++ logs.php
@@ -3,22 +3,23 @@ $page = 'Logs';
 require_once 'lib/databaseHandler/connection.php';
 require_once 'lib/init.php';
 require_once 'lib/no_session_bypass.php';
+require_once 'assets/includes/time_relative.php';
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <?php include_once 'assets/includes/head.php'; ?>
-<script type="text/javascript" src="assets/js/dashboard.js"></script>
 <script>
    $(document).ready(function() {
       var log_count = 10;
 
-      $("#logs").load("lib/log/fetch_log.php", {
-         "datas[]": [log_count]
-      });
-
-      $(document).on("click", ".fetch_more", function() {
+      $("#fetch_more").click(function() {
+         log_count = log_count + 10;
+         console.log(log_count);
+         $("#logs").load("lib/log/fetch_log.php", {
+            "datas[]": [log_count]
+         });
 
       });
 
@@ -46,10 +47,31 @@ require_once 'lib/no_session_bypass.php';
             <div class="card-body">
                <h5 class="card-title">Recent Activity</h5>
                <div id="logs" class="activity">
-                  Loading...
+                  <?php
+
+                  $statement = $pdo->prepare(
+                     "SELECT * FROM logs ORDER BY log_id DESC LIMIT 10"
+                  );
+                  $statement->execute();
+                  $result = $statement->fetchAll();
+                  foreach ($result as $row) {
+                  ?>
+                     <div class="row mb-2">
+                        <div class="col-md-2">
+                           <div class="activite-label">
+                              <?php echo time2str($row['log_date']); ?>
+                           </div>
+                        </div>
+                        <div class="col-md-9">
+                           <div class="activity-content"><strong><?php echo $row['user_email']; ?> </strong><?php echo $row['action']; ?></div>
+                        </div>
+                     </div>
+                  <?php
+                  }
+                  ?>
                </div>
 
-               <button class="btn btn-primary block mt-4" class="fetch_more">Fetch more Logs</button>
+               <button class="btn btn-primary mt-4" id="fetch_more" class="fetch_more">View more</button>
 
             </div>
          </div>
