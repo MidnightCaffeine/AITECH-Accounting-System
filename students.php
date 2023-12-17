a71f83ea948f3e8f88c22e8138393730c23d990e
MidnightCaffeine <jobert.simbre14@gmail.com>
2023-12-18 07:33:23 +0800

    section hanggang z

--- students.php
+++ students.php
@@ -112,6 +112,22 @@ require_once 'lib/no_session_bypass.php';
                                 <option value="G">G</option>
                                 <option value="H">H</option>
                                 <option value="I">I</option>
+                                <option value="J">J</option>
+                                <option value="K">K</option>
+                                <option value="N">N</option>
+                                <option value="M">M</option>
+                                <option value="O">O</option>
+                                <option value="P">P</option>
+                                <option value="Q">Q</option>
+                                <option value="R">R</option>
+                                <option value="S">S</option>
+                                <option value="T">T</option>
+                                <option value="U">U</option>
+                                <option value="V">V</option>
+                                <option value="W">W</option>
+                                <option value="X">X</option>
+                                <option value="Y">Y</option>
+                                <option value="Z">Z</option>
                             </select>
                             <label for="section">Section</label>
                         </div>
